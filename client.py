b96aab09b11016df6cd05408e4dd805f051642f8
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-26 10:39:11 +0100

    client cogiendo IP, PUERTO y LINEA

--- client.py
+++ client.py
@@ -3,25 +3,29 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = sys.argv[3:]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+Mssg = ""
+for word in LINE:
+    Mssg = Mssg + " " + word
+print("Enviando: " + Mssg)
+
+my_socket.send(bytes(Mssg, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
