d5e602c3b181fe0350204f205157222c79446729
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-26 20:13:50 +0100

    añadido Expires y borrado de lista

--- server.py
+++ server.py
@@ -17,8 +17,15 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         newline = line.split(b' ')
         sip_user = newline[1].split(b':')[-1]
         sip_user = sip_user.decode('utf-8')
-        if not sip_user in self.dicc.keys():
-            self.dicc[sip_user] = IP
+        newline = line.split(b'\r')
+        expires = newline[2].split(b':')[-1][1:]
+        expires = expires.decode('utf-8')
+        if int(expires) > 0:
+            if not sip_user in self.dicc.keys():
+                self.dicc[sip_user] = [IP, expires]
+        elif int(expires) == 0:
+            if sip_user in self.dicc.keys():
+                del self.dicc[sip_user]
         print(self.dicc)
 
     def handle(self):
@@ -29,7 +36,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             line = self.rfile.read()
             if line.startswith(b"REGISTER"):
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                print("El cliente nos manda " + line.decode('utf-8'))
+                print("El cliente nos manda " + "\n" + line.decode('utf-8'))
                 self.create_dicc(IP, line)
             # Si no hay más líneas salimos del bucle infinito
             if not line:
