d9ce2a49590650678368ec56411aacf252feee33
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-31 18:00:17 +0100

    sin comprobar expires y borrar

--- server.py
+++ server.py
@@ -7,54 +7,58 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import json
-
+import time
+import calendar
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     dicc = {}
-    def create_dicc(self, sip_user, address, expires):
-        if int(expires) > 0:
+    def create_dicc(self, sip_user, expires, atrib_dicc):
+        if expires > 0:
             if not sip_user in self.dicc.keys():
-                self.dicc[sip_user] = [address, expires]
-        elif int(expires) == 0:
+                self.dicc[sip_user] = atrib_dicc
+        elif expires <= 0:
             if sip_user in self.dicc.keys():
                 del self.dicc[sip_user]
-        print(self.dicc)
 
-    def register2json(self, sip_user):
-        self.tag = []
-        if sip_user in self.dicc.keys():
-            help_dicc = {}
-            for dato in self.dicc[sip_user]:
-                if dato.startswith("127."):
-                    help_dicc["address"] = dato
-                else:
-                    help_dicc["expires"] = dato
-        self.tag.append([sip_user, help_dicc])
-        print(self.tag)
+    def del_user(self, actual_time, expires):
+        for user in self.dicc:
+            atribs = self.dicc[user]
+            if actual_time >= calendar.timegm(expires):
+                del self.dicc[user]
+
+    def register2json(self):
         with open('registered.json', 'w') as outfile_json:
-            json.dump(self.tag, outfile_json, sort_keys=True,
-                      indent=3, separators=(' ', ': '))
+            json.dump(self.dicc, outfile_json, sort_keys=True,
+                      indent=3, separators=(',', ': '))
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         address = self.client_address[0]
+        atrib_dicc = {}
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            if line.startswith(b"REGISTER"):
+            line = line.decode('utf-8');
+            if line.startswith("REGISTER"):
+                print("El cliente nos manda " + "\n" + line)
+                newline = line.split(' ')
+                sip_user = newline[1].split(':')[-1]
+                newline = line.split('\r')
+                expires = int(newline[1].split(':')[-1][1:])
+                expires_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()+expires))
+                atrib_dicc['address'] = address
+                atrib_dicc['expires'] = expires_time
+                self.create_dicc(sip_user, expires, atrib_dicc)
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                print("El cliente nos manda " + "\n" + line.decode('utf-8'))
-                newline = line.split(b' ')
-                sip_user = newline[1].split(b':')[-1]
-                sip_user = sip_user.decode('utf-8')
-                newline = line.split(b'\r')
-                expires = newline[2].split(b':')[-1][1:]
-                expires = expires.decode('utf-8')
-                self.create_dicc(sip_user, address, expires)
-                self.register2json(sip_user)
+                actual_time = time.gmtime(time.time())
+                #self.del_user(actual_time, expires)
+                self.register2json()
+            else:
+                self.wfile.write(b"Peticion recibida\r\n\r\n")
+                print("El cliente nos manda " + "\n" + line)
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
